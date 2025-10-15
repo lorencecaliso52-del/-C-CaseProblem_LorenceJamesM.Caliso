ac574817e9610a665868e951233cafa942bc6ea0
Lorence James M. Caliso <lorencecaliso52@gmail.com>
2025-10-15 12:29:08 +0800

    Add files via upload
    Uploaded C case problem and output screenshot.

---
+++ CALISO_IT112_PIT/LORENCE.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int grade;
+
+    printf("\nName: Lorence James M. Caliso");
+    printf("\nSchool ID Number: 2025304614");
+
+    printf("\n-------------------------------------\n");
+
+    printf("\nEnter your grade (0-100): ");
+    scanf("%d", &grade);
+
+    printf("\n-------------------------------------\n");
+
+    if (grade < 0 || grade > 100) {
+        printf(" Invalid input! Please enter 0 - 100.\n");
+    } 
+    else if (grade >= 90 && grade <= 100) {
+        printf(" Descriptive Rating : Outstanding\n");
+        printf(" Remarks            : PASSED ✅\n");
+    } 
+    else if (grade >= 85 && grade <= 89) {
+        printf(" Descriptive Rating : Very Satisfactory\n");
+        printf(" Remarks            : PASSED ✅\n");
+    } 
+    else if (grade >= 80 && grade <= 84) {
+        printf(" Descriptive Rating : Satisfactory\n");
+        printf(" Remarks            : PASSED ✅\n");
+    } 
+    else if (grade >= 75 && grade <= 79) {
+        printf(" Descriptive Rating : Fairly Satisfactory\n");
+        printf(" Remarks            : PASSED ✅\n");
+    } 
+    else {
+        printf(" Descriptive Rating : Did Not Meet Expectations\n");
+        printf(" Remarks            : FAILED ❌\n");
+    }
+
+    printf("-------------------------------------\n");
+    printf("        Thank you for using this!\n");
+    printf("=====================================\n");
+
+    return 0;
+}
